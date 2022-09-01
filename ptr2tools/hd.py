e5cf44b3bfce33afbff8a2db89ba84898b83349d
jmkd3v <jmk@jmksite.dev>
2022-09-01 16:45:55 -0400

    add dumping

--- ptr2tools/hd.py
+++ ptr2tools/hd.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
 
 import io
+import math
 from enum import Enum
 from dataclasses import dataclass
-from pathlib import Path
-from typing import BinaryIO, List
+from typing import BinaryIO, List, Optional
 
 from .vag import VagFile
 
@@ -18,7 +18,8 @@ class HdChunkType(Enum):
     program = "Prog"
 
 
-class HdChunk: ...
+class HdChunk:
+    ...
 
 
 @dataclass
@@ -34,11 +35,16 @@ class HdVersionChunk(HdChunk):
             minor_version=int.from_bytes(stream.read(1), "little")
         )
 
+    def dump(self, stream: BinaryIO):
+        stream.write(b"\x00" * 2)
+        stream.write(int.to_bytes(self.major_version, 1, "little"))
+        stream.write(int.to_bytes(self.minor_version, 1, "little"))
+
 
 @dataclass
 class HdHeaderChunk(HdChunk):
-    hd_file_size: int
-    bd_file_size: int
+    hd_size: int
+    bd_size: int
     program_chunk_offset: int
     sample_set_chunk_offset: int
     sample_chunk_offset: int
@@ -47,13 +53,21 @@ class HdHeaderChunk(HdChunk):
     @classmethod
     def load(cls, stream: BinaryIO):
         return cls(
-            hd_file_size=int.from_bytes(stream.read(4), "little"),
-            bd_file_size=int.from_bytes(stream.read(4), "little"),
+            hd_size=int.from_bytes(stream.read(4), "little"),
+            bd_size=int.from_bytes(stream.read(4), "little"),
             program_chunk_offset=int.from_bytes(stream.read(4), "little"),
             sample_set_chunk_offset=int.from_bytes(stream.read(4), "little"),
             sample_chunk_offset=int.from_bytes(stream.read(4), "little"),
             vag_info_chunk_offset=int.from_bytes(stream.read(4), "little")
         )
+
+    def dump(self, stream: BinaryIO):
+        stream.write(int.to_bytes(self.hd_size, 4, "little"))
+        stream.write(int.to_bytes(self.bd_size, 4, "little"))
+        stream.write(int.to_bytes(self.program_chunk_offset, 4, "little"))
+        stream.write(int.to_bytes(self.sample_set_chunk_offset, 4, "little"))
+        stream.write(int.to_bytes(self.sample_chunk_offset, 4, "little"))
+        stream.write(int.to_bytes(self.vag_info_chunk_offset, 4, "little"))
 
 
 @dataclass
@@ -89,34 +103,111 @@ class HdVagInfoChunk(HdChunk):
             items=items
         )
 
+    def dump(self, stream: BinaryIO):
+        stream.write(int.to_bytes(len(self.items) - 1, 4, "little"))
+        item_position = 16 + (len(self.items) * 4)
 
-_CHUNK_TYPE_INDEX = {
+        for info_index in range(len(self.items)):
+            stream.write(int.to_bytes(item_position + (8 * info_index), 4, "little"))
+
+        for item in self.items:
+            stream.write(int.to_bytes(item.vag_offset, 4, "little"))
+            stream.write(int.to_bytes(item.sample_rate, 2, "little"))
+            stream.write(b"\x01" if item.loop else b"\x00")
+            stream.write(b"\xFF")
+
+
+@dataclass
+class HdSampleChunk:
+    data: bytes
+
+    @classmethod
+    def load(cls, stream: BinaryIO):
+        return cls(data=stream.read())
+
+    def dump(self, stream: BinaryIO):
+        stream.write(self.data)
+
+
+@dataclass
+class HdSampleSetChunk:
+    data: bytes
+
+    @classmethod
+    def load(cls, stream: BinaryIO):
+        return cls(data=stream.read())
+
+    def dump(self, stream: BinaryIO):
+        stream.write(self.data)
+
+
+@dataclass
+class HdProgramChunk:
+    data: bytes
+
+    @classmethod
+    def load(cls, stream: BinaryIO):
+        return cls(data=stream.read())
+
+    def dump(self, stream: BinaryIO):
+        stream.write(self.data)
+
+
+_CHUNK_TYPE_CLASS = {
     HdChunkType.version: HdVersionChunk,
     HdChunkType.header: HdHeaderChunk,
     HdChunkType.vag_info: HdVagInfoChunk,
-    # HdChunkType.sample: HdSampleChunk,
-    # HdChunkType.sample_set: HdSampleSetChunk
+    HdChunkType.sample: HdSampleChunk,
+    HdChunkType.sample_set: HdSampleSetChunk,
+    HdChunkType.program: HdProgramChunk
 }
+_CHUNK_CLASS_TYPE = {v: k for k, v in _CHUNK_TYPE_CLASS.items()}
 
 
 def _read_chunk(stream: BinaryIO, chunk_type: HdChunkType):
     raw_creator_name = stream.read(4)
     if len(raw_creator_name) != 4:
-        raise EOFError("No more data! :(")
+        raise EOFError("not enough data")
 
-    creator_name = raw_creator_name.decode("ascii")[::-1]
+    if raw_creator_name.decode("ascii")[::-1] != "SCEI":
+        raise ValueError("bad creator name")
+
     if HdChunkType(stream.read(4).decode("ascii")[::-1]) != chunk_type:
         raise ValueError("chunk type doesn't match :(")
 
     chunk_size = int.from_bytes(stream.read(4), "little")
     chunk_stream = io.BytesIO(stream.read(chunk_size - 12))
-    return _CHUNK_TYPE_INDEX[chunk_type].load(chunk_stream)
+    return _CHUNK_TYPE_CLASS[chunk_type].load(chunk_stream)
+
+
+def _write_chunk(stream: BinaryIO, chunk, length: Optional[int] = None):
+    chunk_type: HdChunkType = _CHUNK_CLASS_TYPE[type(chunk)]
+    stream.write(b"SCEI"[::-1])
+    stream.write(chunk_type.value[::-1].encode("ascii"))
+
+    chunk_stream = io.BytesIO()
+    chunk.dump(chunk_stream)
+    chunk_stream.seek(0)
+    chunk_data = chunk_stream.read()
+    del chunk_stream
+
+    if length is None:
+        length = 16 * math.ceil((len(chunk_data) + 12) / 16)
+    pad_amount = (length - 12) - len(chunk_data)
+
+    stream.write(int.to_bytes(length, 4, "little"))
+    stream.write(chunk_data)
+    stream.write(b"\xFF" * pad_amount)
 
 
 @dataclass
 class HdFile:
+    bd_size: int
     version_chunk: HdVersionChunk
     vag_info_chunk: HdVagInfoChunk
+    sample_chunk: HdSampleChunk
+    sample_set_chunk: HdSampleSetChunk
+    program_chunk: HdProgramChunk
 
     @classmethod
     def load(cls, stream: BinaryIO) -> HdFile:
@@ -126,16 +217,60 @@ class HdFile:
         stream.seek(header_chunk.vag_info_chunk_offset)
         vag_info_chunk = _read_chunk(stream, HdChunkType.vag_info)
 
-        # stream.seek(header_chunk.program_chunk_offset)
-        # program_chunk = _read_chunk(stream, HdChunkType.program)
+        stream.seek(header_chunk.sample_chunk_offset)
+        sample_chunk = _read_chunk(stream, HdChunkType.sample)
+
+        stream.seek(header_chunk.sample_set_chunk_offset)
+        sample_set_chunk = _read_chunk(stream, HdChunkType.sample_set)
+
+        stream.seek(header_chunk.program_chunk_offset)
+        program_chunk = _read_chunk(stream, HdChunkType.program)
 
         return cls(
             version_chunk=version_chunk,
-            vag_info_chunk=vag_info_chunk
+            vag_info_chunk=vag_info_chunk,
+            sample_chunk=sample_chunk,
+            sample_set_chunk=sample_set_chunk,
+            program_chunk=program_chunk,
+            bd_size=header_chunk.bd_size
         )
+
+    def dump(self, stream: BinaryIO):
+        start_offset = stream.tell()
+        _write_chunk(stream, self.version_chunk)
+
+        header_offset = stream.tell()
+        stream.write(b"\x00" * 64)  # this will be filled in later
+
+        vag_info_chunk_offset = stream.tell()
+        _write_chunk(stream, self.vag_info_chunk)
+
+        sample_chunk_offset = stream.tell()
+        _write_chunk(stream, self.sample_chunk)
+
+        sample_set_chunk_offset = stream.tell()
+        _write_chunk(stream, self.sample_set_chunk)
+
+        program_chunk_offset = stream.tell()
+        _write_chunk(stream, self.program_chunk)
+
+        end_pos = stream.tell()
+
+        stream.seek(header_offset)
+        _write_chunk(stream, HdHeaderChunk(
+            hd_size=end_pos - start_offset,
+            bd_size=self.bd_size,
+            program_chunk_offset=program_chunk_offset,
+            sample_set_chunk_offset=sample_set_chunk_offset,
+            sample_chunk_offset=sample_chunk_offset,
+            vag_info_chunk_offset=vag_info_chunk_offset
+        ), length=64)
+
+        stream.seek(end_pos)
 
     def get_vags(self, bd_stream: BinaryIO) -> List[VagFile]:
         vag_items = self.vag_info_chunk.items
+        self.vag_info_chunk.dump(io.BytesIO())
         vags = []
         for vag_index, vag_item in enumerate(vag_items):
             if vag_index == len(vag_items) - 1:
